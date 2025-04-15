b6193b887a14661be266f404b42bf9d5b67066d5
sgh <97212710+sgh94@users.noreply.github.com>
2025-04-16 02:53:53 +0900

    layout.tsx 생성

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { Toaster } from 'react-hot-toast';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: '택배기사 물류센터 배치 관리 시스템',
+  description: '택배기사를 특정 물류센터에 배치해주는 서비스',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>
+        {children}
+        <Toaster position="top-right" />
+      </body>
+    </html>
+  );
+}

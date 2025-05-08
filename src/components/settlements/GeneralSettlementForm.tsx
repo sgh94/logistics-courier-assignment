e11472833f1dbdb6a8a4cfd55cd8fe549cc0430a
sgh <97212710+sgh94@users.noreply.github.com>
2025-05-09 01:29:44 +0900

    Add "use client" directive to GeneralSettlementForm

--- src/components/settlements/GeneralSettlementForm.tsx
+++ src/components/settlements/GeneralSettlementForm.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from 'react';
 import { GeneralSettlementRow, CreateGeneralSettlementDTO } from '@/lib/types/settlement';
 import EditableTable from './EditableTable';
